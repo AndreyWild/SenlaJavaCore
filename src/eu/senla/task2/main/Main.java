db7339b416bd76eec553466a7a2402d05643d5ab
AndreyWild <and1rei@mail.ru>
2021-01-20 19:09:21 +0300

    Закинул в реп

---
+++ src/eu/senla/task2/main/Main.java
@@ -0,0 +1,9 @@
+package eu.senla.task2.main;
+
+import eu.senla.task2.rainbow.Rainbow;
+
+public class Main {
+    public static void main(String[] args) {
+        Rainbow rainbow1 = new Rainbow();
+    }
+}
