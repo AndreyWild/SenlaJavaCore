16480bb4a3d643a6696f88b3c43a31efbcfd20ef
AndreyWild <and1rei@mail.ru>
2021-03-18 21:41:27 +0300

    add task14

---
+++ src/eu/senla/task14/ClockSpeedComparator.java
@@ -0,0 +1,16 @@
+package eu.senla.task14;
+
+import java.util.Comparator;
+
+public class ClockSpeedComparator implements Comparator<CPU> {
+    @Override
+    public int compare(CPU cpu1, CPU cpu2) {
+        if (cpu1.getClockSpeed() == cpu2.getClockSpeed()){
+            return 0;
+        }else if (cpu1.getClockSpeed() < cpu2.getClockSpeed()){
+            return -1;
+        }else {
+            return 1;
+        }
+    }
+}
